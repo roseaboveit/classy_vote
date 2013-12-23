9a757f92e6f8ed86b65f78d082aed84bbad3e392
Whitney Levis <ms.whitney.levis@gmail.com>
2013-12-22 16:40:10 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClassyVote::Application.config.secret_key_base = 'f0e8ebb50537cb56c3256d33cda9666e36b56b7f0b5611fe58a8e4e486f64f67c108ebb9aead051877571fc25834cccb872e206639a238699a000034b21cc489'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClassyVote::Application.config.session_store :cookie_store, key: '_classy_vote_session'
