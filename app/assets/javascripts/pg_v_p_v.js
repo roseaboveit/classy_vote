38799915a5a31eaf7d6f7957d5ccee8f3b967ec9
Whitney Levis <ms.whitney.levis@gmail.com>
2014-01-16 15:38:43 -0800

    Assigns H and V for pictograph pvpv

--- app/assets/javascripts/pg_v_p_v.js
+++ app/assets/javascripts/pg_v_p_v.js
@@ -5,14 +5,24 @@ document.addEventListener("DOMContentLoaded", function(){
   var context = canvas.getContext("2d");
   //Scrape basic variables from page
   var pgVEl = document.querySelector("#page_views");
-  var vEl = document.querySelector("#total_visits");
+  var visitsEl = document.querySelector("#total_visits");
   var pvpvEl = document.querySelector("#pvpv");
   var pgV = parseInt(pgVEl.innerHTML, 10);
-  var v = parseInt(vEl.innerHTML, 10);
+  var visits = parseInt(visitsEl.innerHTML, 10);
   var pvpv = parseInt(pvpvEl.innerHTML, 10);
 
   //Helpful Methods
     // How Many Verticals & Horizontals
+    if(Math.sqrt(pvpv) == Math.floor(Math.sqrt(pvpv))){
+      var verticals = Math.sqrt(pvpv) - 1;
+    }
+    else{
+      var verticals = Math.floor(Math.sqrt(pvpv));
+    };
+
+    var horizontals = Math.floor(Math.sqrt(pvpv + verticals)) - 1;
+    console.log(verticals, horizontals);
+
     // How Many Boxes to fill
     // How to Draw in a box
   //Simple-ish Variables to start
