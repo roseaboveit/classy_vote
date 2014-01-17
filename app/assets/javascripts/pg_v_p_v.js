aa9a633ee50cb02788733d08214249dd25c49396
Whitney Levis <ms.whitney.levis@gmail.com>
2014-01-16 21:32:13 -0800

    fix typo

--- app/assets/javascripts/pg_v_p_v.js
+++ app/assets/javascripts/pg_v_p_v.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function(){
     var relX = 0;
     var relY = 0;
     for(var i=0; i < pvpv; i++ ){
-      if (relX > width-tinyWidth) {
+      if (relX > canvas.width-tinyWidth) {
         reset_relX(relX);
         console.log(relX, "relX");
         increment_relY(relY);
